a4c05166f1532d6bfa46fee624b6002d8660ef60
Zabai Armas Herrera <zbrmas@hotmail.com>
2021-10-18 21:01:40 +0100

    Campground new and create

--- Yelpcamp/index.js
+++ Yelpcamp/index.js
@@ -17,6 +17,7 @@ const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 app.get('/', (_, res) => {
   res.render('home');
@@ -25,6 +26,16 @@ app.get('/', (_, res) => {
 app.get('/campgrounds', async (_, res) => {
   const campgrounds = await Campground.find({});
   res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/new', async (req, res) => {
+  res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
 });
 
 app.get('/campgrounds/:id', async (req, res) => {
