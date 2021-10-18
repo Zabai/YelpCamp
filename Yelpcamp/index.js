e8c0f515d3edc643b05cdf44c0f3dfbee572d149
Zabai Armas Herrera <zbrmas@hotmail.com>
2021-10-18 20:23:00 +0100

    Campground model basics

--- Yelpcamp/index.js
+++ Yelpcamp/index.js
@@ -1,12 +1,34 @@
+const Campground = require('./models/campground');
 const express = require('express');
-const app = express();
+const mongoose = require('mongoose');
 const path = require('path');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  autoCreate: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const app = express();
 
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
 
 app.get('/', (_, res) => {
   res.render('home');
+});
+
+app.get('/campground', async (_, res) => {
+  const camp = new Campground({
+    title: 'My Backyard',
+    description: 'Cheap camping!',
+  });
+  await camp.save();
+  res.send(camp);
 });
 
 app.listen(3000, () => {
