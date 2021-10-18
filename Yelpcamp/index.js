544c4e091ff197b5b02fa9a097a9acb8d7643553
Zabai Armas Herrera <zbrmas@hotmail.com>
2021-10-18 21:17:58 +0100

    Feature/Campgrounds Crud (#1)
    * Creating basic Express app
    
    * Campground model basics
    
    * Seeding campgrounds
    
    * Campgrounds index
    
    * Campground show
    
    * Campground new and create
    
    * Campground edit and update
    
    * Campground delete

---
+++ Yelpcamp/index.js
@@ -0,0 +1,66 @@
+const Campground = require('./models/campground');
+const express = require('express');
+const methodOverride = require('method-override');
+const mongoose = require('mongoose');
+const path = require('path');
+const { request } = require('http');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  autoCreate: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const app = express();
+
+app.set('view engine', 'ejs');
+app.set('views', path.join(__dirname, 'views'));
+app.use(methodOverride('_method'));
+app.use(express.urlencoded({ extended: true }));
+
+app.get('/', (_, res) => {
+  res.render('home');
+});
+
+app.get('/campgrounds', async (_, res) => {
+  const campgrounds = await Campground.find({});
+  res.render('campgrounds/index', { campgrounds });
+});
+
+app.get('/campgrounds/new', async (req, res) => {
+  res.render('campgrounds/new');
+});
+
+app.post('/campgrounds', async (req, res) => {
+  const campground = new Campground(req.body.campground);
+  await campground.save();
+  res.redirect(`/campgrounds/${campground._id}`);
+});
+
+app.get('/campgrounds/:id', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/show', { campground });
+});
+
+app.get('/campgrounds/:id/edit', async (req, res) => {
+  const campground = await Campground.findById(req.params.id);
+  res.render('campgrounds/edit', { campground });
+});
+
+app.put('/campgrounds/:id', async (req, res) => {
+  await Campground.findByIdAndUpdate(req.params.id, req.body.campground);
+  res.redirect('/campgrounds');
+});
+
+app.delete('/campgrounds/:id', async (req, res) => {
+  await Campground.findByIdAndDelete(req.params.id);
+  res.redirect('/campgrounds');
+});
+
+app.listen(3000, () => {
+  console.log('Serving on port 3000');
+});
---
+++ Yelpcamp/seeds/index.js
@@ -0,0 +1,28 @@
+const Campground = require('../models/campground');
+const faker = require('faker');
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost:27017/yelp-camp', {
+  autoCreate: true,
+});
+
+const db = mongoose.connection;
+db.on('error', console.error.bind(console, 'connection error:'));
+db.once('open', () => {
+  console.log('Database connected');
+});
+
+const seedDb = async () => {
+  await Campground.deleteMany({});
+  for (let i = 0; i < 50; i++) {
+    const campground = new Campground({
+      title: faker.name.title(),
+      location: faker.address.cityName(),
+    });
+    await campground.save();
+  }
+};
+
+seedDb().then(() => {
+    mongoose.connection.close();
+});
