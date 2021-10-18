deb18aa377401e9614065ab0a66892f61a2db871
Zabai Armas Herrera <zbrmas@hotmail.com>
2021-10-18 21:16:14 +0100

    Campground delete

--- Yelpcamp/index.js
+++ Yelpcamp/index.js
@@ -56,6 +56,11 @@ app.put('/campgrounds/:id', async (req, res) => {
   res.redirect('/campgrounds');
 });
 
+app.delete('/campgrounds/:id', async (req, res) => {
+  await Campground.findByIdAndDelete(req.params.id);
+  res.redirect('/campgrounds');
+});
+
 app.listen(3000, () => {
   console.log('Serving on port 3000');
 });
